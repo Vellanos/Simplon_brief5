7939ea8db285b5b2a1f8cfc9b629ab202b09200b
Vela <davidbak38@gmail.com>
2023-04-07 15:20:12 +0200

    Last Commit

--- gameService.ts
+++ gameService.ts
@@ -4,9 +4,10 @@ import portraitJaina from "./Assets/img/Jaina_anim.gif"
 import portraitGarrosh from "./Assets/img/Garrosh_anim.gif"
 import gemmeMana from "./Assets/img/zyro-image.png"
 import rage from "./Assets/img/zyro_image_(3).png"
+import fin from "./Assets/img/last_hs.png"
 
-
-
+let tempsEcoule:number = 0;
+let intervalId;
 
 export function fPlay (arg1:HTMLCollectionOf<Element>, arg2:HTMLCollectionOf<Element>) {
 
@@ -36,8 +37,9 @@ export function fName (arg1:HTMLCollectionOf<Element>, arg2:HTMLCollectionOf<Ele
     }
 }
 
-export function fChoixPersonnage (arg1:string | null, arg2:HTMLCollectionOf<Element>, arg3:HTMLCollectionOf<Element>,arg4:HTMLInputElement | null, arg5:HTMLInputElement | null) {
+export function fChoixPersonnage (arg1:string | null, arg2:HTMLCollectionOf<Element>, arg3:HTMLCollectionOf<Element>,arg4:HTMLInputElement | null, arg5:HTMLInputElement | null, arg6:HTMLCollectionOf<Element> ) {
 
+    let myHero: object
     arg3[0].classList.remove('choixHero')
 
     for (let i = arg2.length-1; i > -1;){ //Afficher la page 4
@@ -55,6 +57,8 @@ export function fChoixPersonnage (arg1:string | null, arg2:HTMLCollectionOf<Elem
             arg4?.setAttribute("src", `${portraitJaina}`)
             arg5?.setAttribute("src", `${gemmeMana}`)
             arg5?.setAttribute("alt", "Icone de gemme de Mana")
+            arg6[arg6.length-1]?.setAttribute("id", "barreMana")
+            myHero = new Jaina("Jaina", 80, 50, 100)
 
 
         } 
@@ -64,22 +68,66 @@ export function fChoixPersonnage (arg1:string | null, arg2:HTMLCollectionOf<Elem
             arg4?.setAttribute("src", `${portraitGarrosh}`)
             arg5?.setAttribute("src", `${rage}`)
             arg5?.setAttribute("alt", "Icone de rage")
-
+            arg6[arg6.length-1]?.setAttribute("id", "barreRage")
+            myHero = new Garrosh("Garrosh", 10, 10, 10) //100 80 30
+            
         }
+        fRemoveRessource(arg6, myHero)
+        
+}
+
+function fRemoveRessource (arg1:HTMLCollectionOf<Element>, arg2:object) {
+
+    arg1[0]?.setAttribute("value", arg2.faim)
+    arg1[1]?.setAttribute("value", arg2.pv)
+    arg1[2]?.setAttribute("value", arg2.mana || arg2.rage)
+
+    let intervalRessources = setInterval(() => {
+        for (let i = 0; i < arg1.length; i++) {
+            
+            if (arg1[i].value > 50){
+                arg1[i].value -= 10
+            } else if (arg1[i].value <= 50 && arg1[i].value > 15){
+                arg1[i].value -= 6
+            } else if (arg1[i].value <= 15) {
+                arg1[i].value -= 2
+            }
+        }
+    }, 1000);
 
 }
 
-let tempsEcoule:number = 0;
-let intervalId;
+export function fAjoutRessource(arg1:string, arg2:HTMLCollectionOf<Element>) {
+        if (arg1 === "Food" && arg2[0].value > 0){
+            arg2[0].value += 10
+        } else if (arg1 === "Soin" && arg2[1].value > 0){
+            arg2[1].value += 10
+        } else if (arg1 === "Switch" && arg2[2].value > 0) {
+            arg2[2].value += 10
+        }
+}
 
-export function startTimer() {
+export function startTimer(arg1:HTMLCollectionOf<Element>, arg2:HTMLCollectionOf<Element>,arg3:HTMLCollectionOf<Element>,arg4:Element) {
     
     intervalId = setInterval(() => {
     tempsEcoule++;
     afficherTemps();
-    if (tempsEcoule == 10) { // A terme : condition avec pv = 0
+    if (arg1[0].value == 0 && arg1[1].value == 0 && arg1[2].value == 0) { // A terme : condition avec pv = 0
         clearInterval(intervalId);
-        localStorage.setItem('tempsEcoule', tempsEcoule);
+        localStorage.setItem('tempsEcoule', tempsEcoule.toString());
+        let leTemps:any = localStorage.getItem('tempsEcoule')
+        for (let i = arg2.length-1; i > -1;){ //Cacher la page 4
+            arg2[i].classList.add('hide')
+            i = i - 1
+        }
+        for (let i = arg3.length-1; i > -1;){ //Afficher la page 5
+            arg3[i].classList.remove('hide')
+            i = i - 1
+        }
+        document.body.style.backgroundImage = `url(${fin})`;
+        const minutes = Math.floor(leTemps / 60);
+        const secondes = leTemps % 60;
+        arg4.innerHTML = "You played " + `${minutes.toString().padStart(2, '0')}:${secondes.toString().padStart(2, '0')}`;
       }
     }, 1000);
 }
@@ -92,5 +140,48 @@ function afficherTemps() {
         tempsAffiche.textContent = `${minutes.toString().padStart(2, '0')}:${secondes.toString().padStart(2, '0')}`;
     }
   }
-  
 
+export function fRestart () {
+    location.reload();
+}
+
+class Hero {
+  // Propriétés
+  name: string
+  faim: number
+  pv: number
+
+  // Constructor
+  constructor(theName: string, theFaim: number, thePv: number){
+      this.name = theName
+      this.faim = theFaim
+      this.pv = thePv
+  } 
+
+}
+
+class Jaina extends Hero {
+    // Propriétés
+    mana: number
+
+
+    //Constructor
+    constructor(theName: string, theFaim: number, thePv: number, theMana: number){
+        super(theName, theFaim, thePv)
+        this.mana = theMana
+    }
+
+}
+
+class Garrosh extends Hero {
+    // Propriétés
+    rage: number
+
+
+    //Constructor
+    constructor(theName: string, theFaim: number, thePv: number, theRage: number){
+        super(theName, theFaim, thePv)
+        this.rage = theRage
+    }
+
+}
--- script.ts
+++ script.ts
@@ -2,18 +2,41 @@ import { fPlay } from './gameService';
 import { fName } from './gameService';
 import { fChoixPersonnage } from './gameService';
 import { startTimer } from './gameService';
+import { fAjoutRessource } from './gameService';
+import { fRestart } from './gameService';
 
 let page1:HTMLCollectionOf<Element> = document.getElementsByClassName('page_1')
 let page2:HTMLCollectionOf<Element> = document.getElementsByClassName('page_2')
 let page3:HTMLCollectionOf<Element> = document.getElementsByClassName('page_3')
 let page4:HTMLCollectionOf<Element> = document.getElementsByClassName('page_4')
+let page5:HTMLCollectionOf<Element> = document.getElementsByClassName('page_5')
 let personnage:HTMLCollectionOf<Element> = document.getElementsByClassName('personnage')
 let boutonGame:HTMLCollectionOf<Element> = document.getElementsByClassName('boutonGame')
+let barreProgress:HTMLCollectionOf<Element> = document.getElementsByClassName('barre_progress')
+
 
 const play: Element | null = document.querySelector("#play")
 const inputName:HTMLInputElement | null = document.querySelector("#inputName")
 const portrait:Element | null = document.querySelector("#portrait")
 const boutonSwitch:Element | null = document.querySelector("#boutonSwitch")
+const boutonSoin:Element | null = document.querySelector("#boutonSoin")
+const boutonFood:Element | null = document.querySelector("#boutonFood")
+const timerPlayed:Element | null = document.querySelector("#timerPlayed")
+const restart:Element | null = document.querySelector("#restart")
+
+
+
+if (boutonFood) {
+    boutonFood.addEventListener('click', () => fAjoutRessource("Food",barreProgress))
+}
+
+if (boutonSoin) {
+    boutonSoin.addEventListener('click', () => fAjoutRessource("Soin",barreProgress))
+}
+
+if (boutonSwitch) {
+    boutonSwitch.addEventListener('click', () => fAjoutRessource("Switch",barreProgress))
+}
 
 if (play) {
     play.addEventListener('click', () => fPlay(page1,page2))
@@ -35,9 +58,12 @@ if (personnage){
         personnage[i].addEventListener("click",() => {
             localStorage.setItem('idPersonnage', personnage[i].id);
             let choixPersonnage:string | null = localStorage.getItem('idPersonnage')
-            fChoixPersonnage(choixPersonnage, page4, page3, portrait, boutonSwitch)
-            startTimer()
+            fChoixPersonnage(choixPersonnage, page4, page3, portrait, boutonSwitch,barreProgress)
+            startTimer(barreProgress,page4,page5,timerPlayed)
             })
     }
 }
 
+if (restart) {
+    restart.addEventListener('click', () => fRestart())
+}
